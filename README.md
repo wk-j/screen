d65d53cfa656a10d32e0318db9e17bd02553f6b5
wk-j <somnuk.wk@gmail.com>
2019-03-09 00:13:10 +0700

    Added build script

--- README.md
+++ README.md
@@ -1,3 +1,25 @@
 ## Screen
 
 [![NuGet](https://img.shields.io/nuget/v/wk.Screen.svg)](https://www.nuget.org/packages/wk.Screen)
+
+Execute command in remote shell
+
+## Installation
+
+```bash
+dotnet tool install -g wk.Screen
+```
+
+## Usage
+
+Terminal 1
+
+```bash
+wk-screen --init
+```
+
+Terminal 2
+
+```bash
+wk-screen --command ls
+```
--- src/Screen/Program.fs
+++ src/Screen/Program.fs
@@ -3,9 +3,36 @@ open StartProcess
 
 let PS = Processor.StartProcess
 
+type Options = {
+    Init: bool
+    Command: string
+}
+
+let rec parse options args =
+    match args with
+    | "--init" :: xs ->
+        parse { options with Init = true } xs
+    | "--command" :: xs ->
+        match xs with
+        | value :: xss ->
+            parse { options with Command = value } xss
+        | _ -> parse options xs
+    | _ -> options
+
 [<EntryPoint>]
 let main argv =
 
-    PS("echo hello, world!")
+    let options =
+        parse { Init = false; Command = "pwd" }
+              (List.ofArray argv)
 
+    if options.Init then
+        PS("screen -S wk-screen")
+
+    else
+        let template =
+            """/bin/bash -c "screen -S wk-screen -X stuff '{command}'$(echo -ne '\015')" """
+
+        let cmd = template.Replace("{command}", options.Command)
+        PS(cmd)
     0
