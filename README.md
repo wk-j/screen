259bfda1ced655b1c422daf5774691d2b8500715
wk-j <somnuk.wk@gmail.com>
2019-03-08 23:34:02 +0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+## Screen
+
+[![NuGet](https://img.shields.io/nuget/v/wk.Screen.svg)](https://www.nuget.org/packages/wk.Screen)
---
+++ src/Screen/Program.fs
@@ -0,0 +1,11 @@
+﻿open System
+open StartProcess
+
+let PS = Processor.StartProcess
+
+[<EntryPoint>]
+let main argv =
+
+    PS("echo hello, world!")
+
+    0
