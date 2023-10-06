ad7ec594eefcb8ebbcfe688a28135bce7d67b26c
wk <somnuk.wk@gmail.com>
2023-10-06 19:28:46 +0700

    Refactor code to target .NET 6.0

--- src/Screen/Program.fs
+++ src/Screen/Program.fs
@@ -3,28 +3,23 @@ open StartProcess
 
 let PS = Processor.StartProcess
 
-type Options = {
-    Init: bool
-    Command: string
-}
+type Options = { Init: bool; Command: string }
 
 let rec parse options args =
     match args with
-    | "--init" :: xs  | "-i" :: xs ->
-        parse { options with Init = true } xs
-    | "--command" :: xs | "-c" :: xs ->
+    | "--init" :: xs
+    | "-i" :: xs -> parse { options with Init = true } xs
+    | "--command" :: xs
+    | "-c" :: xs ->
         match xs with
-        | value :: xss ->
-            parse { options with Command = value } xss
+        | value :: xss -> parse { options with Command = value } xss
         | _ -> parse options xs
     | _ -> options
 
 [<EntryPoint>]
 let main argv =
 
-    let options =
-        parse { Init = false; Command = "pwd" }
-              (List.ofArray argv)
+    let options = parse { Init = false; Command = "pwd" } (List.ofArray argv)
 
     if options.Init then
         PS("""/bin/bash -c "screen -ls | grep wk-screen | cut -d. -f1 | awk '{print $1}' | xargs kill" """)
@@ -36,4 +31,5 @@ let main argv =
 
         let cmd = template.Replace("{command}", options.Command)
         PS(cmd)
+
     0
